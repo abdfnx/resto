6cf1b57f9c3e8064c3c14a001edb0593d1fc836f
abdfnx <abdfnabd@gmail.com>
2021-12-01 20:48:33 +0300

    add `validation` module with `checkURL` function

---
+++ validation/checkURL.go
@@ -0,0 +1,24 @@
+package validation
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+func checkURL(urlStr string) (string, error) {
+	if urlStr == "" {
+		return "", fmt.Errorf("URL is needed\n")
+	}
+
+	prefixCheck := strings.HasPrefix(urlStr, "http://") || strings.HasPrefix(urlStr, "https://")
+	if !prefixCheck {
+		return "", fmt.Errorf("URL missing protocol or contains invalid protocol\n")
+	}
+
+	if _, err := url.Parse(urlStr); err != nil {
+		return "", fmt.Errorf("URL is invalid\n")
+	}
+
+	return urlStr, nil
+}
