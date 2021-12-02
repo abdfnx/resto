6a6d594d19abb7209e27db43a16966c7061fee5f
abdfnx <abdfnabd@gmail.com>
2021-12-02 21:36:26 +0300

    redesign the modfile, build `tools` modul

--- go.mod
+++ go.mod
@@ -3,9 +3,17 @@ module github.com/abdfnx/rest-it
 go 1.17
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.2
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
+	github.com/muesli/reflow v0.3.0
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
+)
+
+require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
---
+++ tools/errors.go
@@ -0,0 +1,46 @@
+package tools
+
+import (
+	"errors"
+
+	"github.com/AlecAivazis/survey/v2/terminal"
+)
+
+// FlagError is the kind of error raised in flag processing
+type FlagError struct {
+	Err error
+}
+
+func (fe FlagError) Error() string {
+	return fe.Err.Error()
+}
+
+func (fe FlagError) Unwrap() error {
+	return fe.Err
+}
+
+// SilentError is an error that triggers exit code 1 without any error messaging
+var SilentError = errors.New("SilentError")
+
+// CancelError signals user-initiated cancellation
+var CancelError = errors.New("CancelError")
+
+func IsUserCancellation(err error) bool {
+	return errors.Is(err, CancelError) || errors.Is(err, terminal.InterruptErr)
+}
+
+func MutuallyExclusive(message string, conditions ...bool) error {
+	numTrue := 0
+
+	for _, ok := range conditions {
+		if ok {
+			numTrue++
+		}
+	}
+
+	if numTrue > 1 {
+		return &FlagError{Err: errors.New(message)}
+	}
+
+	return nil
+}
