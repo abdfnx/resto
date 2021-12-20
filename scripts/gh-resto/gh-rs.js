9d88040bd4e8ca785be86372104d8a86048501e3
abdfnx <abdfnabd@gmail.com>
2021-12-20 14:53:42 +0300

    update `gh-rs.js` script, use `ad-m/github-push-action` action to push changes

--- scripts/gh-resto/gh-rs.js
+++ scripts/gh-resto/gh-rs.js
@@ -7,8 +7,8 @@ const rm = require("rimraf");
 const mkdirp = require("mkdirp");
 const sh = require("shelljs");
 
-const VERSION_CMD = sh.exec("git describe --abbrev=0 --tags")
-const VERSION_DATE_CMD = sh.exec("cat date.txt")
+const VERSION_CMD = sh.exec("git describe --abbrev=0 --tags");
+const VERSION_DATE_CMD = sh.exec("go run ./build/date.go");
 
 const VERSION = VERSION_CMD.replace("\n", "").replace("\r", "");
 const VERSION_DATE = VERSION_DATE_CMD.replace("\n", "").replace("\r", "");
@@ -22,23 +22,19 @@ async function updateRestoExtension(ghRestoDir) {
 
   const templateReplaced = template
     .replace("CLI_VERSION", VERSION)
-    .replace("CLI_VERSION_DATE", VERSION_DATE)
+    .replace("CLI_VERSION_DATE", VERSION_DATE);
 
-  fs.writeFileSync(
-    path.join(ghRestoDir, "gh-resto"),
-    templateReplaced
-  );
+  fs.writeFileSync(path.join(ghRestoDir, "gh-resto"), templateReplaced);
 }
 
 async function updateExtension() {
   const tmp = path.join(__dirname, "tmp");
   const extensionDir = path.join(tmp, "gh-resto");
+
   mkdirp.sync(tmp);
   rm.sync(extensionDir);
 
-  console.log(
-    `cloning https://github.com/abdfnx/gh-resto to ${extensionDir}`
-  );
+  console.log(`cloning https://github.com/abdfnx/gh-resto to ${extensionDir}`);
 
   await execa("git", [
     "clone",
@@ -51,19 +47,6 @@ async function updateExtension() {
   console.log("updating local git...");
 
   await updateRestoExtension(extensionDir);
-
-  const git = async (args, opts = {}) => {
-    await execa("git", ["-C", extensionDir, ...args], opts);
-  };
-
-  await git(["add", "gh-resto"]);
-  await git(["config", "--local", "core.pager", "cat"]);
-  await git(["diff", "--cached"], { stdio: "inherit" });
-  await git(["commit", "-m", "resto `" + VERSION + "`"]);
-
-  if (process.env.SKIP_GIT_PUSH === undefined) {
-    await git(["push", "origin", "main"]);
-  }
 }
 
 updateExtension().catch((err) => {
