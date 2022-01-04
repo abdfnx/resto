9f896915e12265deaa5187a5587a7974a62b9290
abdfnx <abdfnabd@gmail.com>
2022-01-04 21:13:08 +0300

    update windows `bfs`, create `scripts/shell` dir for docker

--- scripts/bfs.ps1
+++ scripts/bfs.ps1
@@ -1,7 +1,7 @@
 # Build From Source
 $loc = "$HOME\AppData\Local\resto"
 
-go run build/date.go >> date.txt
+go run scripts/date.go >> date.txt
 
 $LATEST_VERSION=git describe --abbrev=0 --tags
 $DATE=cat date.txt
